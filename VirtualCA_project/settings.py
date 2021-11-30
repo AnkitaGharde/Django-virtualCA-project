32a6081be89f5256bf50eb4d766c3cca1c0c4159
AnkitaGharde <84709511+AnkitaGharde@users.noreply.github.com>
2021-11-30 13:03:29 +0530

    Update settings.py

--- VirtualCA_project/settings.py
+++ VirtualCA_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-_cx(g_n#k@9dm3xbdal7#2dnm*-7p4cyl*ynl9(wp(h5byx8o4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','virtual-ca-django-app.herokuapp.com']
 
 
 # Application definition
